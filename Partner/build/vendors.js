b8c605656f9c520b2d93d101929f50053c5efc83
uris <urisvirott@yahoo.com>
2020-04-17 13:30:35 -0700

    Update

---
+++ Partner/build/vendors.js
@@ -0,0 +1,121 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["vendors~main"],{
+
+/***/ "./designDependencies.js":
+/*!*******************************!*\
+  !*** ./designDependencies.js ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+
+const designs = {}
+
+// The packages are passed in through a template
+
+
+module.exports = designs
+
+
+/***/ }),
+
+/***/ "framer-package-loader.js!./node_modules/@framer/framer.device-skin-apple-ipad-air-silver/build/index.js":
+/*!********************************************************************************************************************************************************************************************************************************!*\
+  !*** /Applications/Framer X.app/Contents/Resources/Server/node_modules/@framerjs/build-library/build/Compiler/loaders/framer-package-loader.js!./node_modules/@framer/framer.device-skin-apple-ipad-air-silver/build/index.js ***!
+  \********************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+class DeviceSkin {
+    constructor() {
+        this.image = "device.png";
+        this.imageWidth = 1856;
+        this.imageHeight = 2608;
+        this.padding = 40;
+        this.background = "black";
+    }
+}
+exports.DeviceSkin = DeviceSkin;
+//# sourceMappingURL=index.js.map
+
+
+/***/ }),
+
+/***/ "framer-package-loader.js!./node_modules/@framer/framer.device-skin-apple-ipad-pro-silver/build/index.js":
+/*!********************************************************************************************************************************************************************************************************************************!*\
+  !*** /Applications/Framer X.app/Contents/Resources/Server/node_modules/@framerjs/build-library/build/Compiler/loaders/framer-package-loader.js!./node_modules/@framer/framer.device-skin-apple-ipad-pro-silver/build/index.js ***!
+  \********************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+class DeviceSkin {
+    constructor() {
+        this.image = "device.png";
+        this.imageWidth = 2448;
+        this.imageHeight = 3432;
+        this.padding = 40;
+        this.background = "black";
+    }
+}
+exports.DeviceSkin = DeviceSkin;
+//# sourceMappingURL=index.js.map
+
+
+/***/ }),
+
+/***/ "framer-package-loader.js!./node_modules/@framer/framer.device-skin-apple-ipad-pro-space-gray/build/index.js":
+/*!************************************************************************************************************************************************************************************************************************************!*\
+  !*** /Applications/Framer X.app/Contents/Resources/Server/node_modules/@framerjs/build-library/build/Compiler/loaders/framer-package-loader.js!./node_modules/@framer/framer.device-skin-apple-ipad-pro-space-gray/build/index.js ***!
+  \************************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+class DeviceSkin {
+    constructor() {
+        this.image = "device.png";
+        this.imageWidth = 2448;
+        this.imageHeight = 3432;
+        this.padding = 40;
+        this.background = "black";
+    }
+}
+exports.DeviceSkin = DeviceSkin;
+//# sourceMappingURL=index.js.map
+
+
+/***/ }),
+
+/***/ "framer-package-loader.js!./node_modules/@framer/framer.device-skin-apple-macbook-pro/build/index.js":
+/*!****************************************************************************************************************************************************************************************************************************!*\
+  !*** /Applications/Framer X.app/Contents/Resources/Server/node_modules/@framerjs/build-library/build/Compiler/loaders/framer-package-loader.js!./node_modules/@framer/framer.device-skin-apple-macbook-pro/build/index.js ***!
+  \****************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+class DeviceSkin {
+    constructor() {
+        this.image = "device.png";
+        this.imageWidth = 3820;
+        this.imageHeight = 2320;
+        this.padding = 40;
+        this.background = "black";
+    }
+}
+exports.DeviceSkin = DeviceSkin;
+//# sourceMappingURL=index.js.map
+
+
+/***/ })
+
+}]);
