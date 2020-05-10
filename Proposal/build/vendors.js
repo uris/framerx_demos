455a9d00c93c206a6928b47ef883c1ebd6212eb4
Uris Dacosta <urisvirott@yahoo.com>
2020-05-10 16:37:18 -0700

    Propsal Setup

---
+++ Proposal/build/vendors.js
@@ -0,0 +1,46 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["vendors~main"],{
+
+/***/ "./designDependencies.js":
+/*!*******************************!*\
+  !*** ./designDependencies.js ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+
+const designs = {}
+
+// The packages are passed in through a template
+
+
+module.exports = designs
+
+
+/***/ }),
+
+/***/ "framer-package-loader.js!./node_modules/@framer/framer.device-skin-apple-macbook-pro/build/index.js":
+/*!****************************************************************************************************************************************************************************************************************************!*\
+  !*** /Applications/Framer X.app/Contents/Resources/Server/node_modules/@framerjs/build-library/build/Compiler/loaders/framer-package-loader.js!./node_modules/@framer/framer.device-skin-apple-macbook-pro/build/index.js ***!
+  \****************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+class DeviceSkin {
+    constructor() {
+        this.image = "device.png";
+        this.imageWidth = 3820;
+        this.imageHeight = 2320;
+        this.padding = 40;
+        this.background = "black";
+    }
+}
+exports.DeviceSkin = DeviceSkin;
+//# sourceMappingURL=index.js.map
+
+
+/***/ })
+
+}]);
